ffbd7a22e417c80346d9e82393c52f5d9b6f0f61
Aman Singh <amy07singh1985@gmail.com>
2024-08-08 15:14:18 +0530

    Add files via upload

---
+++ two_arrays.java
@@ -0,0 +1,20 @@
+package kth_element;
+import java.util.*;
+public class two_arrays {
+	public int mergesort()
+	{
+		return 1;
+		
+	}
+	public long kthElement(int k, int arr1[], int arr2[]) {
+		return 1;
+    }
+	public static void main(String args[])
+	{
+		int k = 2;
+		int[] arr1 = {5,5,8,8,8,9,11,11,11,11,11};
+		int[] arr2 = {4,4,4,4,6,8,9,9,9,11,13};
+		two_arrays t1 = new two_arrays();
+		System.out.println(t1.kthElement(k, arr1, arr2));
+	}
+}
